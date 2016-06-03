630f04457cb79acb92b74a1387834449dc894435
Jano <Jano@Janillo>
2016-06-03 18:37:21 +0200

    Método de conexión creado

--- src/BBDD.java
+++ src/BBDD.java
@@ -1,6 +1,11 @@
 
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.Statement;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -15,5 +20,27 @@ public class BBDD {
     public static Statement st;
     
     
+    /**
+     * Este método gener la conexión a la base de datos a través de tres parámetros
+     * Si se realiza correctamente saldrá un mensaje de éxito, de lo contrario uno de fracaso.
+     * Con este método también inicializamos el atributo 'STATEMENT' que se empleará en las consultas.
+     * @param dbName Nombre de la base de datos a la que queremos conectarnos
+     * @param user  Usuario que se empleará en la conexión
+     * @param pass  Contraseña del usuario
+     * @throws Exception 
+     */
+    public static void conectarse(String dbName, String user, String pass) throws Exception {
+      
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conexion = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/" + dbName, user, pass);
+            JOptionPane.showMessageDialog(null, "Se ha iniciado la conexión con el servidor de forma exitosa");
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(BBDD.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "NO e ha iniciado la conexión");
+        }
+        st=(Statement) conexion.createStatement();
+        
+    }
 
 }
