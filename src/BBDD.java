5432181360ecee01dd960a3b6c7865d5bf94f96e
Jano <Jano@Janillo>
2016-06-03 18:33:44 +0200

    Atributos creados

--- src/BBDD.java
+++ src/BBDD.java
@@ -1,8 +1,19 @@
 
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+
 /**
  *
  * @author Jano
  */
 public class BBDD {
+
+    /**
+     * Atributos a los que invocaremos desde el proyecto una vez la librería esté creada
+     */
+    public static Connection conexion;
+    public static Statement st;
     
+    
+
 }
